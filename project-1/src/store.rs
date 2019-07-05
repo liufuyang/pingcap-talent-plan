599cba538cd85b68b37444bdf8d53c7b70927fe5
Fuyang Liu <fuyangl@spotify.com>
2019-07-05 21:18:15 +0200

    impl memory kv store

--- project-1/src/store.rs
+++ project-1/src/store.rs
@@ -1,19 +1,41 @@
-pub struct KvStore {}
+use std::collections::HashMap;
 
+pub struct KvStore {
+    store: HashMap<String, String>
+}
+
+/// A store that keeps key-value pairs in memory
+/// ```rust
+/// # use kvs::KvStore;
+/// let mut store = KvStore::new();
+///
+/// store.set("key1".to_owned(), "value1".to_owned());
+/// assert_eq!(store.get("key1".to_owned()), Some("value1".to_owned()));
+///
+/// store.remove("key1".to_owned());
+/// assert_eq!(store.get("key1".to_owned()), None);
+/// ```
 impl KvStore {
+
+    /// Creat a key value store
     pub fn new() -> KvStore {
-        panic!();
+        KvStore {
+            store: HashMap::new()
+        }
     }
 
+    /// Set key value to store
     pub fn set(&mut self, key: String, value: String) {
-        panic!();
+        self.store.insert(key, value);
     }
 
+    /// Get value by a key from store
     pub fn get(&self, key: String) -> Option<String> {
-        panic!();
+        self.store.get(&key).cloned()
     }
 
+    /// Remove key value from store
     pub fn remove(&mut self, key: String) {
-        panic!()
+        self.store.remove(&key);
     }
 }
